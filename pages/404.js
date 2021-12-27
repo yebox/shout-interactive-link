7cba0190baa86454d5fa98aa8022db91d991b713
Atolagbe Elisha <atolagbeelisha001>
2021-12-27 23:13:27 +0100

    img path fix

--- pages/404.js
+++ pages/404.js
@@ -7,9 +7,9 @@ const NotFound = () => {
         <header className="flex items-center justify-center">
           <button className="border body_heavy border-white py-6 px-7 ml-auto text-white">Contact us</button>
         </header>
-        <img className="max-w-[1337px] w-full relative top-1/4 md:top-1/3 -translate-y-2/3 left-1/2 -translate-x-1/2" src="404-text.svg"></img>
-        <img className="absolute left-0 bottom-0 max-h-[350px] md:max-h-[773px]" src="404-tree.svg"></img>
-        <img className="absolute bottom-0 right-16 max-h-[256] h-[150px] md:h-auto" src="404-house.svg"></img>
+        <img className="max-w-[1337px] w-full relative top-1/4 md:top-1/3 -translate-y-2/3 left-1/2 -translate-x-1/2" src="/404-text.svg"></img>
+        <img className="absolute left-0 bottom-0 max-h-[350px] md:max-h-[773px]" src="/404-tree.svg"></img>
+        <img className="absolute bottom-0 right-16 max-h-[256] h-[150px] md:h-auto" src="/404-house.svg"></img>
         <div className="text-center flex flex-col items-center z-50 relative mt-[130px]">
           <div className="flex items-center">
             <p className="pr-[39px] title_heavy text-white">Party Popper</p>
